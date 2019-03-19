eead3ad3d08c6cfbb8be957ef99771d6415b2796
David Enrique Prieto Melendez <david.enrique.prieto.melendez@everis.com>
2019-03-19 09:25:09 +0100

    Adde Effects file and Try Signup action

---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,15 @@
+import { Effect, Actions } from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+
+import * as AuthActions from './auth.actions';
+
+@Injectable()
+export class AuthEffects {
+    @Effect()
+    authSignup = this.actions$
+        .ofType(AuthActions.TRY_SIGNUP);
+
+    constructor(private actions$: Actions) {
+
+    }
+}
